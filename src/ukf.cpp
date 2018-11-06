bb2c96f8cf0e302f934c00265222e5b72e470c39
DrBoltzmann <mark@idezo.ch>
2018-11-06 20:18:18 +0000

    first commit

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -49,26 +49,109 @@ UKF::UKF() {
   
   /**
   TODO:
-
   Complete the initialization. See ukf.h for other member properties.
-
   Hint: one or more values initialized above might be wildly off...
   */
+  
+  n_x_ = x_.size();
+  // Augmented state dimension
+  n_aug_ = n_x_ + 2; // We will create 2 * n_aug_ + 1 sigma points 
+  // Number of sigma points
+  n_sig_ = 2 * n_aug_ + 1;
+  // Set the predicted sigma points matrix dimentions
+  Xsig_pred_ = MatrixXd(n_x_, n_sig_);
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;
+  // Weights of sigma points
+  weights_ = VectorXd(n_sig_);
+  // Measurement noise covariance matrices initialization
+  R_radar_ = MatrixXd(3, 3);
+  R_radar_ << std_radr_*std_radr_, 0, 0,
+              0, std_radphi_*std_radphi_, 0,
+              0, 0,std_radrd_*std_radrd_;
+  R_lidar_ = MatrixXd(2, 2);
+  R_lidar_ << std_laspx_*std_laspx_,0,
+              0,std_laspy_*std_laspy_;
 }
 
 UKF::~UKF() {}
-
 /**
  * @param {MeasurementPackage} meas_package The latest measurement data of
  * either radar or laser.
  */
+void UKF::NormAng(double *ang) {
+   while (*ang > M_PI) *ang -= 2. * M_PI;
+   while (*ang < -M_PI) *ang += 2. * M_PI;
+}
+
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   /**
   TODO:
-
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  
+  // CTRV Model, x_ is [px, py, vel, ang, ang_rate]
+  if (!is_initialized_) {
+  // Initial covariance matrix
+    P_ << 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 1, 0, 0,
+          0, 0, 0, 1, 0,
+          0, 0, 0, 0, 1;
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      // Convert radar from polar to cartesian coordinates and initialize state.
+      float rho = measurement_pack.raw_measurements_[0]; // range
+      float phi = measurement_pack.raw_measurements_[1]; // bearing
+      float rho_dot = measurement_pack.raw_measurements_[2]; // velocity of rho
+      // Coordinates convertion from polar to cartesian
+      float px = rho * cos(phi); 
+      float py = rho * sin(phi);
+      float vx = rho_dot * cos(phi);
+      float vy = rho_dot * sin(phi);
+      float v  = sqrt(vx * vx + vy * vy);
+      x_ << px, py, v, 0, 0;
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      // We don't know velocities from the first measurement of the LIDAR, so, we use zeros
+      x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0, 0;
+      // Deal with the special case initialisation problems
+      if (fabs(x_(0)) < EPS and fabs(x_(1)) < EPS){
+		x_(0) = EPS;
+		x_(1) = EPS;
+	  }
+    }
+    
+    // Initialize weights
+    weights_(0) = lambda_ / (lambda_ + n_aug_);
+    for (int i = 1; i < weights_.size(); i++) {
+        weights_(i) = 0.5 / (n_aug_ + lambda_);
+    }
+    
+    // Save the initiall timestamp for dt calculation
+    time_us_ = measurement_pack.timestamp_;
+    // Done initializing, no need to predict or update
+    is_initialized_ = true;
+    //cout << "Init" << endl;
+    //cout << "x_" << x_ << endl;
+    return;
+  }
+  
+  // Calculate the timestep between measurements in seconds
+  double dt = (measurement_pack.timestamp_ - time_us_);
+  dt /= 1000000.0; // convert micros to s
+  time_us_ = measurement_pack.timestamp_;
+  Prediction(dt);
+  //cout << "predict:" << endl;
+  //cout << "x_" << endl << x_ << endl;
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
+	  //cout << "Radar " << measurement_pack.raw_measurements_[0] << " " << measurement_pack.raw_measurements_[1] << endl;
+      UpdateRadar(measurement_pack);
+    }
+  if (measurement_pack.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+	  //cout << "Lidar " << measurement_pack.raw_measurements_[0] << " " << measurement_pack.raw_measurements_[1] << endl;
+      UpdateLidar(measurement_pack);
+  }
 }
 
 /**
@@ -79,38 +162,71 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 void UKF::Prediction(double delta_t) {
   /**
   TODO:
-
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  
+  double delta_t2 = delta_t*delta_t;
+  // Augmented mean vector
+  VectorXd x_aug = VectorXd(n_aug_);
+  // Augmented state covarience matrix
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+  // Sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, n_sig_);
+  // Fill the matrices
+  x_aug.fill(0.0);
+  x_aug.head(n_x_) = x_;
+  P_aug.fill(0);
+  P_aug.topLeftCorner(n_x_,n_x_) = P_;
+  P_aug(5,5) = std_a_*std_a_;
+  P_aug(6,6) = std_yawdd_*std_yawdd_;
+  // Square root of P matrix
+  MatrixXd L = P_aug.llt().matrixL();
+  // Create sigma points
+  Xsig_aug.col(0) = x_aug;
+  double sqrt_lambda_n_aug = sqrt(lambda_+n_aug_); // Save some computations
+  VectorXd sqrt_lambda_n_aug_L;
+  for(int i = 0; i < n_aug_; i++) {
+	sqrt_lambda_n_aug_L = sqrt_lambda_n_aug * L.col(i);
+    Xsig_aug.col(i+1)        = x_aug + sqrt_lambda_n_aug_L;
+    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt_lambda_n_aug_L;
 }
 
 /**
  * Updates the state and the state covariance matrix using a laser measurement.
  * @param {MeasurementPackage} meas_package
  */
+
+
+
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
   /**
   TODO:
-
   Complete this function! Use lidar data to update the belief about the object's
   position. Modify the state vector, x_, and covariance, P_.
-
   You'll also need to calculate the lidar NIS.
   */
+  
+
 }
 
 /**
  * Updates the state and the state covariance matrix using a radar measurement.
  * @param {MeasurementPackage} meas_package
  */
+
+
+
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
   /**
   TODO:
-
   Complete this function! Use radar data to update the belief about the object's
   position. Modify the state vector, x_, and covariance, P_.
-
   You'll also need to calculate the radar NIS.
   */
+  
+  
+  
+  
+  
 }
